3daf7daf66a7681ced1234b39c4452738f24dcef
Antonio Expósito <aexpber314@g.educaand.es>
2025-01-29 19:35:59 +0100

    Trabajo 29 de enero de 2025

---
+++ Plantilla.py
@@ -0,0 +1,18 @@
+"""Escribir una función que reciba como parámetro una cadena de palabras separadas por espacios (frase)
+ y devuelva, como resultado, cuántas palabras de más de 4 letras tiene la cadena dada.
+ """
+
+# Importación de librerías:
+from os import system # Importo el comando "system" de la librería "os".
+
+# Declaración de variables:
+
+
+
+# Definición de funciones:
+
+
+
+# Programa principal.
+
+system("cls")
